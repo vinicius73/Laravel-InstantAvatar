69a3e01349402e2b0e62c8b8417a2758f8041317
Vinicius <luiz.vinicius73@gmail.com>
2014-08-16 15:38:07 -0300

    init

---
+++ src/Vinicius73/LaravelInstantavatar/LaravelInstantavatarServiceProvider.php
@@ -0,0 +1,45 @@
+<?php namespace Vinicius73\LaravelInstantavatar;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelInstantavatarServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('vinicius73/laravel-instantavatar', 'instantavatar');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
+
+}
