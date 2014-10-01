241bf2a499493ff25bd58fe5a1eeeb473dd6488b
Vinicius <luiz.vinicius73@gmail.com>
2014-10-01 19:17:30 -0300

    Demo

--- readme.md
+++ readme.md
@@ -52,3 +52,30 @@ $avatar->display();
 ```php
 IAvatar::random('Vinicius')->save('public::avatars/vinicius.png')->display();
 ```
+
+## Configuration
+
+```php
+return array(
+    'width'    => 150,
+    'heigth'   => 150,
+    'fontsize' => 80, // in px
+    'chars'    => 2, // Character limit for every image
+    'font' => array( // You can register or remove more sources with ease.
+        'InstantAvatar::Comfortaa-Regular.ttf',
+        'InstantAvatar::Roboto-Regular.ttf',
+        'InstantAvatar::Ubuntu-Regular.ttf',
+        'InstantAvatar::DroidSans.ttf'
+    ),
+    'overlay'  => 'InstantAvatar::glass.png', // A watermark that overlays the image
+    'flat'     => false // Enables the flat mode
+);
+```
+## Demo
+### Default
+![Instant Avatar](http://i.imgur.com/UZBqmXG.png)
+
+### Flat 
+Color based in [avatarly](https://github.com/lucek/avatarly)
+
+![Instant Avatar Flat](http://i.imgur.com/sBk5GHZ.png)
--- src/config/config.php
+++ src/config/config.php
@@ -1,16 +1,15 @@
 <?php
-
 return array(
-    'width'    => 150,
-    'heigth'   => 150,
-    'fontsize' => 80,
-    'chars'    => 2,
-    'font' => array(
-        'InstantAvatar::Comfortaa-Regular.ttf',
-        'InstantAvatar::Roboto-Regular.ttf',
-        'InstantAvatar::Ubuntu-Regular.ttf',
-        'InstantAvatar::DroidSans.ttf'
-    ),
-    'overlay'  => 'InstantAvatar::glass.png',
-    'flat'     => false
+   'width'    => 150,
+   'heigth'   => 150,
+   'fontsize' => 80,
+   'chars'    => 2, // Character limit for every image
+   'font'     => array( // You can register or remove more sources with ease.
+      'InstantAvatar::Comfortaa-Regular.ttf',
+      'InstantAvatar::Roboto-Regular.ttf',
+      'InstantAvatar::Ubuntu-Regular.ttf',
+      'InstantAvatar::DroidSans.ttf'
+   ),
+   'overlay'  => 'InstantAvatar::glass.png', // A watermark that overlays the image
+   'flat'     => false // Enables the flat mode
 );
